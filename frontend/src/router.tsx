e8d6428d9b95ff01b2fc8170c9bb79a4e4f776b0
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-09-21 17:40:58 -0300

    Configura rotas

---
+++ frontend/src/router.tsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from 'react-router-dom'
+import LandingPage from './pages/LandingPage.tsx'
+import NotFoundPage from './pages/NotFoundPage.tsx'
+import HomePage from './pages/HomePage.tsx'
+
+export const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <LandingPage />,
+    errorElement: <NotFoundPage />,
+  },
+  {
+    path: '/home',
+    element: <HomePage />
+  }
+])
