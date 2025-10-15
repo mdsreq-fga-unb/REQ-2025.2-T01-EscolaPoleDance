5ed49644e4ae175ed8c37a766d13e13f3030a85d
Leonardo Bezerra <leonardoalvesbezerra@gmail.com>
2025-10-15 00:41:07 -0300

    feat: create user login route

--- backend/src/routes/user_routes.js
+++ backend/src/routes/user_routes.js
@@ -1,10 +1,66 @@
+require('dotenv').config();
 const express = require('express');
 const router = express.Router();
+const jwt = require('jsonwebtoken');
 const userController = require('../controllers/user_controllers');
+const db = require('../models');
 
-router.get('/login', (req, res) => {
-    res.send("Pagina de login");
-})
+// GET /api/users/login - User login
+router.get('/login', async (req, res) => {
+    try {
+        const { email, password } = req.body;
+
+        if (!user || !password) {
+            res.status(401).json({ error: "Preencha todos os campos." });
+        }
+
+        // Check if user with privded email exists 
+        const user = await db.User.findOne({
+            where: { email: email }
+        });
+
+        if (!user) {
+            return res.status(401).json({ error: "Credenciais inválidas." });
+        }
+        
+        // Compare user provided password with stored hashed password to verify user identity
+        const match = await bcrypt.compare(password, user.password);
+
+        if (!match) {
+            return res.status(401).json({ error: "Credenciais inválidas" });
+        }
+
+        // Generate token with user info. This token will be used to validade if the user is logged in and their role/permissions
+        const payload = {
+            id: user.id,
+            role: user.role
+        };
+
+        const token = jwt.sign(
+            payload,
+            process.env.JWT_SECRET,
+            { expiresIn: process.env.JWT_EXPIRES_IN || '1d' }
+        );
+
+        // Send user login token and user info
+        res.status(200).json({
+            token,
+            user: {
+                id: user.id,
+                email: user.email,
+                firstName: user.firstName,
+                role: user.role
+            }
+        });
+
+    } catch (error) {
+        console.log("Erro ao gerar token de login: " + error);
+        res.status(500).json({ error: "Ocorreu um erro interno ao efetuar o login." });
+    }
+    
+});
+
+
 
 // USER CONTROLLERS -------------------------------
  
