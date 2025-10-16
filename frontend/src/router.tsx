8a93b94af61fd87bac9603ddd28b7d9a27b50858
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-10-16 00:05:32 -0300

    #11 feat: Set up initial structure for auth pages

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,8 @@ import { createBrowserRouter } from 'react-router-dom'
 import LandingPage from './pages/LandingPage.tsx'
 import NotFoundPage from './pages/NotFoundPage.tsx'
 import HomePage from './pages/HomePage.tsx'
+import SignupPage from './pages/SignupPage.tsx'
+import LoginPage from './pages/LoginPage.tsx'
 
 export const router = createBrowserRouter([
   {
@@ -12,5 +14,13 @@ export const router = createBrowserRouter([
   {
     path: '/home',
     element: <HomePage />
-  }
+  },
+  {
+    path: '/signup',
+    element: < SignupPage />
+  },
+  {
+    path: '/login',
+    element: <LoginPage />
+  },
 ])
