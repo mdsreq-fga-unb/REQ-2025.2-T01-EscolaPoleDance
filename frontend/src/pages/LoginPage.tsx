fe3769df613619eadf643a6cb50853ab5088ce11
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-10-16 01:59:03 -0300

    #11 feat: Add responsive layout for auth screens

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,16 +1,22 @@
 export default function LoginPage() {
     return (
-        <div className="w-screen min-h-screen flex ">
-            <div className="w-1/2 min-h-screen relative">
+        <div className="max-w-[2100px] mx-auto w-screen min-h-screen flex ">
+
+            <div className="hidden xl:block w-1/2 min-h-screen relative">
                 <img src="/example-image.jpg" alt="Imagem" className="absolute w-full h-full object-cover" />
                 <div className="absolute inset-0 bg-fuchsia-700 opacity-80 mix-blend-darken pointer-events-none" />
                 <div className="absolute bottom-0 w-full flex">
-                    <p className="text-6xl font-bold text-zinc-100 p-10">
+                    <p className="text-5xl font-bold text-fuchsia-50 p-10">
                         Entre e descubra a força e a confiança que só o pole dance pode te trazer.
                     </p>
+                </div>
+
             </div>
+
+            <div className="w-screen xl:w-1/2 min-h-screen bg-fuchsia-50 flex items-center justify-center">
+                <div className="w-50 h-50 bg-amber-300"></div>
             </div>
-            <div className="w-1/2 min-h-screen bg-fuchsia-50 flex items-center justify-center"></div>
+
         </div>
     )
 }
