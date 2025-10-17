f39f4b42ea80e75b856bea1379b5e7974b39e75d
Afrontoso <victor2lra@gmail.com>
2025-10-17 18:50:23 -0300

    #12 feat: Update Footer component to use anchor tags for links for improved accessibility

--- frontend/src/components/layout/Footer.tsx
+++ frontend/src/components/layout/Footer.tsx
@@ -9,17 +9,28 @@ const Footer: React.FC = () => {
             © Lady's Escola de Pole Dance – CNPJ: 12.345.678/0001-90.
           </p>
           <p className="text-white text-sm font-medium font-['Montserrat'] leading-7">
-            <span className="underline cursor-pointer transition-colors">
+            <a
+              href="https://github.com/mdsreq-fga-unb/REQ-2025.2-T01-EscolaPoleDance"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline cursor-pointer hover:text-pink-200 transition-colors"
+            >
               Desenvolvido por estudantes da Universidade de Brasília (UnB) - Campus Gama
-            </span>
+            </a>
             <span> | </span>
-            <span className="underline cursor-pointer hover:text-fuchsia-pink-200 transition-colors">
+            <a
+              href="/politica-de-privacidade"
+              className="underline cursor-pointer hover:text-pink-200 transition-colors"
+            >
               Política de Privacidade
-            </span>
+            </a>
             <span> | </span>
-            <span className="underline cursor-pointer hover:text-fuchsia-pink-200 transition-colors">
+            <a
+              href="/termos-de-uso"
+              className="underline cursor-pointer hover:text-pink-200 transition-colors"
+            >
               Termos de Uso
-            </span>
+            </a>
           </p>
         </div>
       </div>
