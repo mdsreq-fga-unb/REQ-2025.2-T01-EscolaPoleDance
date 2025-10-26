a346170a36f4e4fb2d5e3b0b0d79ca9f4dad6c27
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-10-26 11:10:20 -0300

    12 style: Fine-tune layout and color adjustments for HeroSection

--- frontend/src/pages/LandingPage.tsx
+++ frontend/src/pages/LandingPage.tsx
@@ -1,20 +1,20 @@
-import Navbar from "../components/layout/Navbar";
-import heroImage from "../assets/img/hero/hero-img.png";
 import Footer from "@/components/layout/Footer";
-import { Button } from "@/components/ui/button";
-import BenefitsCarousel from "@/components/ui/BenefitsCarousel";
-import joemalAbdon from "../assets/img/about/joemalAbdon.jpg";
-import TestimonialCarousel from "@/components/ui/TestimonialCarousel";
-import InfoCard from "@/components/ui/InfoCard";
-import { Link } from "react-router-dom";
-import { FaWhatsapp } from "react-icons/fa";
 import {
   Accordion,
   AccordionContent,
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import BenefitsCarousel from "@/components/ui/BenefitsCarousel";
+import { Button } from "@/components/ui/button";
 import ButtonWhatsapp from "@/components/ui/ButtonWhatsapp";
+import InfoCard from "@/components/ui/InfoCard";
+import TestimonialCarousel from "@/components/ui/TestimonialCarousel";
+import { FaWhatsapp } from "react-icons/fa";
+import { Link } from "react-router-dom";
+import joemalAbdon from "../assets/img/about/joemalAbdon.jpg";
+import heroImage from "../assets/img/hero/hero-img.png";
+import Navbar from "../components/layout/Navbar";
 
 export default function LandingPage() {
   return (
@@ -38,14 +38,14 @@ export default function LandingPage() {
 export function HeroSection() {
   return (
     <section
-      className="relative w-full mt-20 min-h-[calc(100vh-5rem)] bg-gradient-to-r from-fuchsia-pink-700 via-fuchsia-pink-900 to-fuchsia-pink-950 font-[Montserrat] text-fuchsia-pink-50 
-                 flex flex-col md:flex-row justify-between items-center overflow-hidden"
+      className="relative w-full mt-20 min-h-[50vh] sm:min-h-[60vh] md:min-h-[70vh] bg-gradient-to-r from-fuchsia-pink-800 via-fuchsia-pink-900 to-fuchsia-pink-950 text-fuchsia-pink-50 
+                 flex flex-col md:flex-row justify-between items-center z-0"
     >
-      <div className="absolute inset-0 md:left-1/2 md:top-0 md:w-1/2 h-full">
+      <div className="absolute md:left-1/2 md:top-0 md:w-1/2 h-full">
         <img
           src={heroImage}
           alt="Ginasta de Pole Dance"
-          className="w-full h-full object-cover mix-blend-luminosity bg-gradient-to-r from-fuchsia-pink-950 to-transparent opacity-55"
+          className="w-full h-full object-cover mix-blend-luminosity [mask-image:linear-gradient(to_right,transparent,black)]"
         />
       </div>
 
@@ -53,26 +53,26 @@ export function HeroSection() {
         className="relative z-10 text-left md:text-left flex flex-col items-start md:items-start gap-8 
                       p-8 md:p-[90px] h-full justify-center"
       >
-        <h1
+        <span
           className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl 
                        leading-tight sm:leading-tight md:leading-tight lg:leading-tight
-                        font-medium text-fuchsia-pink-50 drop-shadow-lg max-w-2xl md:max-w-3xl"
+                        font-medium text-fuchsia-pink-50 max-w-2xl md:max-w-3xl"
         >
           Ajudando <span className="font-bold">mulheres</span> a conquistar sua{" "}
           <span className="font-bold">melhor versão</span>
-        </h1>
+        </span>
 
         <div className="flex flex-col sm:flex-row gap-4 justify-center md:justify-start mt-4">
           <Button
             size="lg"
-            className="bg-white text-fuchsia-pink-950 hover:bg-gray-100"
+            className="bg-fuchsia-pink-50 text-fuchsia-pink-950 hover:bg-fuchsia-pink-100 cursor-pointer"
           >
             Marque uma aula experimental
           </Button>
           <Button
             variant="outline"
             size="lg"
-            className="border-white text-white bg-transparent hover:bg-white hover:text-fuchsia-pink-950"
+            className="border-fuchsia-pink-50 text-fuchsia-pink-50 bg-transparent hover:bg-fuchsia-pink-100 hover:text-fuchsia-pink-950 cursor-pointer"
           >
             Faça sua matrícula
           </Button>
@@ -400,4 +400,3 @@ export function FAQSection() {
     </section>
   );
 }
-
