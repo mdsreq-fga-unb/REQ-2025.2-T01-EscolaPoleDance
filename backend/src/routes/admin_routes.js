07e86ea20a71d51b03fade1f13c4ac9d0bf590fc
Leonardo Bezerra <leonardoalvesbezerra@gmail.com>
2025-11-04 23:07:51 -0300

    feat: add package admin routes

--- backend/src/routes/admin_routes.js
+++ backend/src/routes/admin_routes.js
@@ -4,6 +4,7 @@ const userController = require('../controllers/user_controllers');
 const classController = require('../controllers/class_controllers');
 const scheduleController = require('../controllers/schedule_controllers');
 const bookingController = require('../controllers/booking_controllers');
+const packageController = require('../controllers/package_controllers');
 const { isLoggedIn, isAdmin } = require('../middleware/auth_middleware');
 
 router.use(isLoggedIn, isAdmin);
@@ -16,7 +17,7 @@ router.use(isLoggedIn, isAdmin);
 router.get('/users', userController.getAllUsers);
 
 // POST /api/admin/createUser - creates a new user
-router.get('/createUser', userController.createUser);
+router.post('/createUser', userController.createUser);
 
 // DELETE /api/admin/users/:id/delete - delete a specific user
 router.delete('/users/:id/delete', userController.deleteUser);
@@ -24,6 +25,25 @@ router.delete('/users/:id/delete', userController.deleteUser);
 // CLASSES ---
 // GET /api/admin/classes - list every class
 router.get('/classes', classController.getAllClasses);
+
+// PACKAGES ---
+// POST /api/admin/packages/createPackage - creates a new package
+router.post('/packages/createPackage', packageController.createPackage);
+
+// GET /api/packages - list every package
+router.get('/packages', packageController.getAllPackages);
+
+// GET /api/packages/:id - list a specific package
+router.get('/packages/:id', packageController.getPackageById);
+
+// GET /api/packages/user/:id - list a user's package
+router.get('/packages/user/:id', packageController.getPackageByUserId);
+
+// PUT /api/packages/:id/update - update a package
+router.put('/packages/:id/update', packageController.updatePackage);
+
+// DELETE /api/packages/:id/delete - delete a package
+router.delete('/packages/:id/delete', packageController.deletePackage);
 
 // ---------------------------------------
 
