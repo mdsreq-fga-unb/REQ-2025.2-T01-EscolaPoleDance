8dbb4d5d6710186cbecf51775b2a15af176e188a
LuizFarias21 <luizclaudiofarias21@gmail.com>
2025-11-14 01:17:07 -0300

    #11 feat: Integrate the navbar component into signup and login page

--- frontend/src/pages/LoginPage.tsx
+++ frontend/src/pages/LoginPage.tsx
@@ -1,22 +1,29 @@
+import Navbar from "@/components/layout/Navbar";
+
 export default function LoginPage() {
     return (
-        <div className="max-w-[2100px] mx-auto w-screen min-h-screen flex ">
+        <>
+            <Navbar/>
+            <div className="max-w-[2100px] mx-auto w-screen min-h-screen flex ">
 
-            <div className="hidden xl:block w-1/2 min-h-screen relative">
-                <img src="/example-image.jpg" alt="Imagem" className="absolute w-full h-full object-cover" />
-                <div className="absolute inset-0 bg-fuchsia-700 opacity-80 mix-blend-darken pointer-events-none" />
-                <div className="absolute bottom-0 w-full flex">
-                    <p className="text-5xl font-bold text-fuchsia-50 p-10">
-                        Entre e descubra a força e a confiança que só o pole dance pode te trazer.
-                    </p>
+                <div className="hidden xl:block w-1/2 min-h-screen relative">
+                    <img src="/example-image.jpg" alt="Imagem" className="absolute w-full h-full object-cover" />
+                    <div className="absolute inset-0 bg-fuchsia-700 opacity-80 mix-blend-darken pointer-events-none" />
+                    <div className="absolute bottom-0 w-full flex">
+                        <p className="text-5xl font-bold text-fuchsia-50 p-10">
+                            Entre e descubra a força e a confiança que só o pole dance pode te trazer.
+                        </p>
+                    </div>
+
+                </div>
+
+                <div className="w-screen xl:w-1/2 min-h-screen bg-fuchsia-50 flex items-center justify-center">
+                    <div className="w-50 h-50 bg-amber-300"></div>
                 </div>
 
             </div>
+        </>
+        
 
-            <div className="w-screen xl:w-1/2 min-h-screen bg-fuchsia-50 flex items-center justify-center">
-                <div className="w-50 h-50 bg-amber-300"></div>
-            </div>
-
-        </div>
     )
 }
--- frontend/src/pages/SignupPage.tsx
+++ frontend/src/pages/SignupPage.tsx
@@ -1,21 +1,25 @@
+import Navbar from "@/components/layout/Navbar";
+
 export default function SignupPage() {
     return (
-        <div className="max-w-[2100px] mx-auto w-screen min-h-screen flex">
+        <>
+        <Navbar/>
+            <div className="max-w-[2100px] mx-auto w-screen min-h-screen flex">
+                <div className="w-full xl:w-1/2 min-h-screen bg-fuchsia-50 flex items-center justify-center">
+                    <div className="w-50 h-50 bg-amber-300"></div>
+                </div>
 
-            <div className="w-full xl:w-1/2 min-h-screen bg-fuchsia-50 flex items-center justify-center">
-                <div className="w-50 h-50 bg-amber-300"></div>
-            </div>
-
-            <div className="hidden xl:block w-1/2 min-h-screen relative">
-                <img src="/example-image.jpg" alt="Imagem" className="absolute w-full h-full object-cover" />
-                <div className="absolute inset-0 bg-fuchsia-700 opacity-80 mix-blend-darken pointer-events-none" />
-                <div className="absolute bottom-0 w-full flex">
-                    <p className="text-5xl font-bold text-fuchsia-50 p-10">
-                        Corpo, poder e confiança em cada movimento.
-                    </p>
+                <div className="hidden xl:block w-1/2 min-h-screen relative">
+                    <img src="/example-image.jpg" alt="Imagem" className="absolute w-full h-full object-cover" />
+                    <div className="absolute inset-0 bg-fuchsia-700 opacity-80 mix-blend-darken pointer-events-none" />
+                    <div className="absolute bottom-0 w-full flex">
+                        <p className="text-5xl font-bold text-fuchsia-50 p-10">
+                            Corpo, poder e confiança em cada movimento.
+                        </p>
+                    </div>
                 </div>
             </div>
+        </>
 
-        </div>
     )
 }
