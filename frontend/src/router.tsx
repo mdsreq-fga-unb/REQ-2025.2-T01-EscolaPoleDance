1caca878d568d8d581ad04ca94da874d4a5e388d
Afrontoso <victor2lra@gmail.com>
2025-11-16 21:49:33 -0300

    feat: Add AboutPage route to the router configuration

--- frontend/src/router.tsx
+++ frontend/src/router.tsx
@@ -2,6 +2,7 @@ import { createBrowserRouter } from "react-router-dom";
 import LandingPage from "./pages/LandingPage/LandingPage.tsx";
 import NotFoundPage from "./pages/NotFoundPage.tsx";
 import HomePage from "./pages/HomePage.tsx";
+import AboutPage from "./pages/AbaoutPage/AboutPage.tsx";
 
 export const router = createBrowserRouter([
   {
@@ -12,6 +13,10 @@ export const router = createBrowserRouter([
   {
     path: "/home",
     element: <HomePage />,
+  },
+  {
+    path: "/about",
+    element: <AboutPage />,
   },
   {
     path: "/404",
