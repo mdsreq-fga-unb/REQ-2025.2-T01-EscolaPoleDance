6ce7f603cd014355106bd79b6f6596307c34745b
Afrontoso <victor2lra@gmail.com>
2025-11-16 21:49:59 -0300

    # 66 feat: Create AboutPage and AboutSection components

---
+++ frontend/src/pages/AbaoutPage/AboutPage.tsx
@@ -0,0 +1,14 @@
+import Navbar from "@/components/layout/Navbar";
+import Footer from "@/components/layout/Footer";
+import AboutSection from './section/AboutSection';
+
+
+export default function AboutPage() {
+    return (
+        <>
+            <Navbar />
+            <AboutSection />
+            <Footer />
+        </>
+    );
+}
